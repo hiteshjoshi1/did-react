92886d965a2ff141382234c3071a5a04066e314f
Olga Kravtsova <olga.maker@gmail.com>
2019-09-11 13:31:31 +1000

    Drizzle Events

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,10 +1,35 @@
 import SimpleStorage from "./contracts/SimpleStorage.json";
 import EthereumDIDRegistry from "./contracts/EthereumDIDRegistry.json";
+// import Web3 from "web3";
+
+// // Init contract with Web3
+// const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+// web3.eth.getAccounts().then(console.log);
+// const contract = new web3.eth.Contract(
+//   SimpleStorage.abi,
+//   "0x72E034FEF2262EE690Bfa6D04b29EF3dAF0b6683",
+//   { data: SimpleStorage.deployedBytecode }
+// );
+
+// // Alternative to Drizzle Events
+// contract
+//   .getPastEvents("allEvents", {
+//     filter: { address: "0xe9f304F332bd4c604D2Fbf0F94A5EaE721ba2b12" }
+//   })
+//   .then(data => console.log("gor all events", data));
 
 const options = {
-  contracts: [SimpleStorage, EthereumDIDRegistry],
+  contracts: [EthereumDIDRegistry, SimpleStorage], // An instance of a Web3 contract
   events: {
-    SimpleStorage: ["StorageSet"],
+    SimpleStorage: [
+      {
+        eventName: "StorageSet",
+        eventOptions: {
+          fromBlock: 0,
+          toBlock: "latest"
+        }
+      }
+    ],
     EthereumDIDRegistry: ["DIDOwnerChanged", "DIDAttributeChanged"]
   },
   polls: {
